2fae6df2feb45729fd06d22c7e0214a2d946df37
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-24 20:48:10 +0200

    13_Multiply Two Numbers with JavaScript

---
+++ 13_Multiply Two Numbers with JavaScript.js
@@ -0,0 +1,18 @@
+// Multiply Two Numbers with JavaScript
+// We can also multiply one number by another.
+
+// JavaScript uses the * symbol for multiplication of two numbers.
+
+// Example
+
+// const myVar = 13 * 13;
+// myVar would have the value 169.
+
+// Change the 0 so that product will equal 80.
+
+// Get Help
+// Tests
+// Waiting:The variable product should be equal to 80.
+// Waiting:You should use the * operator.
+
+const product = 8 * 10;
