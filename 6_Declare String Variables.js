1df375ea1360d0c0e50ccbb3fa0a362736757c87
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-24 20:36:36 +0200

    6_Declare String Variables

---
+++ 6_Declare String Variables.js
@@ -0,0 +1,19 @@
+// Declare String Variables
+// Previously you used the following code to declare a variable:
+
+// var myName;
+// But you can also declare a string variable like this:
+
+// var myName = "your name";
+// "your name" is called a string literal. A string literal, or string, is a series of zero or more characters enclosed in single or double quotes.
+
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+// Get Help
+// Tests
+// Waiting:myFirstName should be a string with at least one character in it.
+// Waiting:myLastName should be a string with at least one character in it.
+
+
+var myFirstName = "YourFirstName";
+var myLastName = "YourLastName";
