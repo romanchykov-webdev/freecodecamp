e4c711cef42e1082f9ee45ffc46c73ab52e5a714
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-24 20:46:26 +0200

    12_Subtract One Number from Another with JavaScript

---
+++ 12_Subtract One Number from Another with JavaScript.js
@@ -0,0 +1,18 @@
+// Subtract One Number from Another with JavaScript
+// We can also subtract one number from another.
+
+// JavaScript uses the - symbol for subtraction.
+
+// Example
+
+// const myVar = 12 - 6;
+// myVar would have the value 6.
+
+// Change the 0 so the difference is 12.
+
+// Get Help
+// Tests
+// Waiting:The variable difference should be equal to 12.
+// Waiting:You should only subtract one number from 45.
+
+const difference = 45 - 33;
