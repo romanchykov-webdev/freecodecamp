8a6c29901c2c07d9c4bb305c0497bc50ecd6aa68
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-24 21:03:28 +0200

    24_Compound Assignment With Augmented Division

---
+++ 24_Compound Assignment With Augmented Division.js
@@ -0,0 +1,26 @@
+// Compound Assignment With Augmented Division
+// The /= operator divides a variable by another number.
+
+// myVar = myVar / 5;
+// Will divide myVar by 5. This can be rewritten as:
+
+// myVar /= 5;
+// Convert the assignments for a, b, and c to use the /= operator.
+
+// Get Help
+// Tests
+// Waiting:a should equal 4.
+// Waiting:b should equal 27.
+// Waiting:c should equal 3.
+// Waiting:You should use the /= operator for each variable.
+// Waiting:You should not modify the code above the specified comment.
+
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /=12;
+b /=4;
+c /=11;
