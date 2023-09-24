ef838129bccd78ba2816b8cc287e3b9daf154588
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-24 21:00:55 +0200

    22_ompound Assignment With Augmented Subtraction

---
+++ 22_Compound Assignment With Augmented Subtraction.js
@@ -0,0 +1,25 @@
+// Compound Assignment With Augmented Subtraction
+// Like the += operator, -= subtracts a number from a variable.
+
+// myVar = myVar - 5;
+// will subtract 5 from myVar. This can be rewritten as:
+
+// myVar -= 5;
+// Convert the assignments for a, b, and c to use the -= operator.
+
+// Get Help
+// Tests
+// Waiting:a should equal 5.
+// Waiting:b should equal -6.
+// Waiting:c should equal 2.
+// Waiting:You should use the -= operator for each variable.
+// Waiting:You should not modify the code above the specified comment.
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -=6;
+b -=15;
+c -=1;
