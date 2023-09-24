9343908a165b910bfb00008efbed31aea3045aa6
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-24 20:56:50 +0200

    19_Divide One Decimal by Another with JavaScript

---
+++ 19_Divide One Decimal by Another with JavaScript.js
@@ -0,0 +1,12 @@
+// Divide One Decimal by Another with JavaScript
+// Now let's divide one decimal by another.
+
+// Change the 0.0 so that quotient will equal to 2.2.
+
+// Get Help
+// Tests
+// Waiting:The variable quotient should equal 2.2
+// Waiting:You should use the / operator to divide 4.4 by 2
+// Waiting:The quotient variable should only be assigned once
+
+const quotient = 4.4 / 2.0; // Change this line
