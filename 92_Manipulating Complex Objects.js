ca947f5db3127f033d7d0cd9b2527daee9b058d4
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-09-30 21:29:55 +0200

    92_Manipulating Complex Objects

---
+++ 92_Manipulating Complex Objects.js
@@ -0,0 +1,54 @@
+// Manipulating Complex Objects
+// Sometimes you may want to store data in a flexible Data Structure. A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+
+// Here's an example of a complex data structure:
+
+// const ourMusic = [
+//   {
+//     "artist": "Daft Punk",
+//     "title": "Homework",
+//     "release_year": 1997,
+//     "formats": [ 
+//       "CD", 
+//       "Cassette", 
+//       "LP"
+//     ],
+//     "gold": true
+//   }
+// ];
+// This is an array which contains one object inside. The object has various pieces of metadata about an album. It also has a nested formats array. If you want to add more album records, you can do this by adding records to the top level array. Objects hold data in a property, which has a key-value format. In the example above, "artist": "Daft Punk" is a property that has a key of artist and a value of Daft Punk.
+
+// Note: You will need to place a comma after every object in the array, unless it is the last object in the array.
+
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+// Get Help
+// Tests
+// Waiting:myMusic should be an array
+// Waiting:myMusic should have at least two elements
+// Waiting:The elements in the myMusic array should be objects
+// Waiting:Your object in myMusic should have at least 4 properties
+// Waiting:Your object in myMusic should contain the property artist which is a string
+// Waiting:Your object in myMusic should contain the property title which is a string
+// Waiting:Your object in myMusic should contain the property release_year which is a number
+// Waiting:Your object in myMusic should contain a formats property which is an array
+// Waiting:formats should be an array of strings with at least two elements
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": ["CD", "8T", "LP"]
+    },
+    // Add your new album object here
+    {
+      "artist": "Daft Punk",
+      "title": "Discovery",
+      "release_year": 2001,
+      "formats": ["CD", "Cassette", "LP"]
+    }
+  ];
+  
+  // Test to ensure the new album has been added
+  console.log(myMusic);
+  
